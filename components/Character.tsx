fa7e4fdef96ad7a59a16250f4082b491080f5f03
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-06-13 02:50:16 +0100

    feat(character.tsx):refactor the code

--- components/Character.tsx
+++ components/Character.tsx
@@ -12,7 +12,7 @@ type CharacterProps = {
 export default function Character({ character }: { character: CharacterProps }) {
  const { id, image, location, name, species, status } = character;
   return (
-    <div className='flex justify-center items-center gap-y-3  p-4 w-72 h-44 gap-2 py-4'>
+    <div className='flex justify-center bg-gray-900 rounded-lg text-white m-3 items-center gap-y-3  p-4 w-72 h-44 gap-2 py-4'>
         <img className='h-24 rounded-md' src={image} alt="" />
         <div className='text-xs flex flex-col py-10'>
           <h1>{name}</h1>
--- components/Characters.tsx
+++ components/Characters.tsx
@@ -6,17 +6,41 @@ type CharacterProps = {
   name: string;
   image: string;
   id: number;
-  status : string;
-  species : string;
-  location : string;
-  
+  status: string;
+  species: string;
+  location: string;
 };
+
+const styles = {
+  button:
+    "py-2 px-10 rounded-3xl my-9 hover:bg-gray-800  text-center font-mono font-bold bg-black text-white",
+};
+
+
 
 type Props = {};
 
 export default function Characters({}: Props) {
-  const fetchCharacters = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+  const [page, setPage] = useState(7);
+
+  const pageController = (arithemeticValue:string)=>{
+    if(page<1){
+      setPage(1)
+    }
+    if(arithemeticValue ==="add"){
+      setPage(page+1)
+    }
+    if(arithemeticValue ==="subtract"){
+      setPage(page-1)
+    } 
+
+
+  }
+  const fetchCharacters = async ({ queryKey }: any) => {
+    console.log(queryKey);
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
     const data = await response.json();
     return data.results.map((character: any) => ({
       id: character.id,
@@ -28,18 +52,36 @@ export default function Characters({}: Props) {
     }));
   };
 
-  const { data, isLoading, isError } = useQuery<CharacterProps[]>("characters", fetchCharacters);
+  const { data, isLoading, isError } = useQuery<CharacterProps[]>(
+    ["characters", page],
+    fetchCharacters
+  );
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error fetching data</div>;
 
   return (
-    <div className=" font-bold font-mono py-2 grid grid-cols-4 w-full justify-center items-center">
-     {data?.map((character) => (
-    
-       <Character key={character.id} character={character} />
-       
-    ))}
+    <div>
+      <div className=" font-bold font-mono my-2 rounded-lg grid grid-cols-4 w-full justify-center items-center">
+        {data?.map((character) => (
+          <Character key={character.id} character={character} />
+        ))}
+      </div>
+      <div className="flex items-center justify-center gap-4">
+        <button
+          disabled={page<2}
+          className={styles.button}
+          onClick={() => pageController("subtract")}
+        >
+          Previous
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => pageController("add")}
+        >
+          Next page
+        </button>
+      </div>
     </div>
   );
 }
